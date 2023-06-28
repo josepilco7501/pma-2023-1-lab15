5cfe051a2324b4da70ca6747a1a97f5f0653b87a
josepilco <jose.pilco@tecsup.edu.pe>
2023-06-28 10:43:56 -0500

    Initial Commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Jose Pilco on 28/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
